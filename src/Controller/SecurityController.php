3387750d6eb5ea68541ae796897762c454e47d76
APLItsFlauline <florian.rauline@centrale-marseille.fr>
2022-02-19 21:28:55 +0100

    Logout (Il manque les boutons de connexion et de déconnexion mais je sais pas ou les mettre)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -44,4 +44,9 @@ class SecurityController extends AbstractController
     public function login(){
         return $this->render('security/login.html.twig');
     }
+
+    /**
+     * @Route("/deconnexion",name="security_logout")
+     */
+    public function logout() {}
 }
